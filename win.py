2410ffc40ad8b0b0a58ef68576949af5578d963c
nisalrenuja <nrenuja@gmail.com>
2024-12-11 16:03:47 +0530

    add win file

--- win.py
+++ win.py
@@ -1,1 +1,8 @@
-404: Not Found
+import os
+
+def main():
+    print("Welcome to the Python Script served from the ASCII Art Server!")
+    print(f"Your current working directory is: {os.getcwd()}")
+
+if __name__ == "__main__":
+    main()
